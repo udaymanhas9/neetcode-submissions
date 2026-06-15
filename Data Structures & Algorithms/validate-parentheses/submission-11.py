4378f2774e84eb0e5557b440ca3c1cd25fac1786
udaymanhas9 <uday1903@yahoo.com>
2026-06-15 11:39:17 +0100

    Add: validate-parentheses - submission-11

---
+++ Data Structures & Algorithms/validate-parentheses/submission-11.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dic = {')': '(', ']':'[', '}':'{'}
+        stack = deque()
+        if len(s) % 2 != 0:
+            return False
+        for bracket in s:
+            if bracket in ('(','[','{'):
+                stack.append(bracket)
+            else:
+                if stack:
+                    left = stack.pop()
+                else:
+                    return False
+        
+                if  left != dic[bracket]:
+                    return False
+        
+        return True if not stack else False
+                
